66b15fa32df1916eec8f72ce35ad346b8c42835c
YIFan Chen <rdgt321@gmail.com>
2012-11-07 22:43:23 +0800

    Add captcha validate for p.nju.edu.cn and bras.nju.edu.cn

--- pNJU/config.py
+++ pNJU/config.py
@@ -4,6 +4,7 @@ PREFERENCE_FILENAME = 'preference'
 SESSION_FILENAME = 'session'
 URL = "http://p.nju.edu.cn/portal.do"
 IMG_URL = "http://p.nju.edu.cn/portal/img.html"
+BRAS_IMG_URL = "http://bras.nju.edu.cn/selfservice/img.html"
 USER_AGENT = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)"
 #SESSION_ID = "imdevelopedbyclippitakazlt"
 LOGIN_STATS_URL = "http://pnjuservice.dayanjia.com/1/login/"
